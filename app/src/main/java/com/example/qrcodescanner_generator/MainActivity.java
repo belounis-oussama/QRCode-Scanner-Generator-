b8030832c84c495ec6d38045f459ca5039f25137
belounis-oussama <contact.elprofessordz@gmail.com>
2022-09-28 21:29:37 +0100

    QR

--- app/src/main/java/com/example/qrcodescanner_generator/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner_generator/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.qrcodescanner_generator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button generate_btn,scan_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWidget();
+
+        
+    }
+
+    private void initWidget() {
+        generate_btn=findViewById(R.id.generate_btn);
+        scan_btn=findViewById(R.id.scan_btn);
     }
 }
